014c91162c5ae8046516108ed56ba8b7afa2f097
Ken Messmer <kenmessmerjr@gmail.com>
2020-12-25 16:22:28 -0600

    Working with strings

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-character_name = "Billy Dee Williams"
-character_age = 89
-is_male = False
-print("There once was a man named " + character_name + ", ")
-print("he was " + str(character_age) + " years old. ")
-print("He really liked the name " + character_name + ", ")
-print("but didn't like being " + str(character_age) + ".")
+phrase = "Giraffe Academy"
+print(phrase.replace("Giraffe", "Elephant"))
+
+
+
