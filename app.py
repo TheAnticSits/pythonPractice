b303b121107585d204d2553f4c8398d582feb41f
Ken Messmer <kenmessmerjr@gmail.com>
2020-12-27 17:21:21 -0600

    working with lists copy sort count reverse

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 
-lucky_numbers = [4, 8, 15, 6, 23, 42]
-friends = ["Kevin", "Karen", "Jim", "Oscar", "Toby"]
-friends.extend(lucky_numbers)
-friends.append("creed")
-friends.insert(1, "Kelly")
-friends.remove("Jim")
-friends.pop()
-friends.clear()
+lucky_numbers = [4, 8, 15, 6, 100, 123, 2, 311, 23, 42]
+friends = ["Kevin", "Karen", "Jim", "Jim", "Oscar", "Toby"]
+
+lucky_numbers.sort()
+
+friends2 = friends.copy()
+friends2.sort()
+
+print(lucky_numbers)
+print(friends.count("Jim"))
+print(friends2)
 print(friends)
+lucky_numbers.reverse()
+print(lucky_numbers)
