1493edbbe387f946dc284b4762a1d3f9677b9dba
Ken Messmer <kenmessmerjr@gmail.com>
2020-12-27 15:25:36 -0600

    List and index

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
 
-color = input("Enter a color: ")
-plural_noun = input("Enter a plural noun: ")
-celebrity = input("Enter a celebrity: ")
-
-print("Roses are " + color)
-print(plural_noun + " are blue")
-print("I love " + celebrity)
+friends = ["Kevin", "Karen", "Jim", "Oscar", "Toby"]
+friends[1] = "Mike"
+print(friends)
