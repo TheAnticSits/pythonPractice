4bd94be0858a57794a896269bb4f85a241b6afb7
Ken Messmer <kenmessmerjr@gmail.com>
2020-12-27 11:21:43 -0600

    build calculator adding input from user.  covers converting to int and float

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 
-name = input("Enter your name: ")
-age = input("Enter your age: ")
-print("Hello " + name + "!  You are " + age + ".")
 
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
 
+result = int(num1) + float(num2)
+
+print(result)
