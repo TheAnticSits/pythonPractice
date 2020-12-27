a195576b433789c6e69da36b4f4c6660e8402c85
Ken Messmer <kenmessmerjr@gmail.com>
2020-12-27 17:13:36 -0600

    working with lists extend append insert remove pop clear

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 
+lucky_numbers = [4, 8, 15, 6, 23, 42]
 friends = ["Kevin", "Karen", "Jim", "Oscar", "Toby"]
-friends[1] = "Mike"
+friends.extend(lucky_numbers)
+friends.append("creed")
+friends.insert(1, "Kelly")
+friends.remove("Jim")
+friends.pop()
+friends.clear()
 print(friends)
