9e51b453dc9c7181b22e9f85047ee7334717c82d
Ken Messmer <kenmessmerjr@gmail.com>
2020-12-27 19:14:43 -0600

    working with tuples

--- app.py
+++ app.py
@@ -1,15 +1,5 @@
 
-lucky_numbers = [4, 8, 15, 6, 100, 123, 2, 311, 23, 42]
-friends = ["Kevin", "Karen", "Jim", "Jim", "Oscar", "Toby"]
+coordinates = [(4, 5), (30,45)]
+test = coordinates[1]
 
-lucky_numbers.sort()
-
-friends2 = friends.copy()
-friends2.sort()
-
-print(lucky_numbers)
-print(friends.count("Jim"))
-print(friends2)
-print(friends)
-lucky_numbers.reverse()
-print(lucky_numbers)
+print(test[1])
