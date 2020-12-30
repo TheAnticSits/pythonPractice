7e60123db4f3071f6c499d46e157150ed0c9c7d2
Ken Messmer <kenmessmerjr@gmail.com>
2020-12-29 22:00:03 -0600

    working with ranint import.

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+import random
 
 name = input("What is your name? ")
 age = input("What is your age? ")
+languages = random.randint(0, 5)
+
 def sayhi(name, age):
     print("Hello " + name + ",")
     print("You are " + age + " years old.")
+    print("My guess is you know " + str(languages) + " programming languages.")
 
 sayhi(name, age)
