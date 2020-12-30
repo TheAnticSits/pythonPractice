97b7d542c3d96d0fc8f39cacd2ceb45e2d5a719c
Ken Messmer <kenmessmerjr@gmail.com>
2020-12-29 23:15:06 -0600

    Self learning turtles from the documentation

--- app.py
+++ app.py
@@ -1,12 +1,41 @@
+import turtle
 import random
+from turtle import Turtle
 
-name = input("What is your name? ")
-age = input("What is your age? ")
-languages = random.randint(0, 5)
+turtle = Turtle()
 
-def sayhi(name, age):
-    print("Hello " + name + ",")
-    print("You are " + age + " years old.")
-    print("My guess is you know " + str(languages) + " programming languages.")
+turtle.speed(10)
 
-sayhi(name, age)
+turtle.up()
+turtle.left(180)
+turtle.forward(150)
+turtle.right(90)
+turtle.forward(300)
+turtle.left(180)
+turtle.down()
+
+
+def draw_circle():
+
+    for j in range(3):
+        def change_color():
+            R = random.random()
+            B = random.random()
+            G = random.random()
+            turtle.color(R, G, B)
+
+        for i in range(120):
+            turtle.down()
+            turtle.forward(50)
+            turtle.left(8)
+            turtle.backward(40)
+            turtle.right(5)
+            change_color()
+
+        turtle.up()
+        turtle.right(55)
+        turtle.forward(70)
+
+
+
+draw_circle()
