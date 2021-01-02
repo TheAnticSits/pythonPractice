d933c1bc73fe43acf16a2fd130e3d7baddea7649
Ken Messmer <kenmessmerjr@gmail.com>
2021-01-01 21:00:42 -0600

    elif practice

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 
-is_male = True
+is_male = False
 is_tall = True
 
 if is_male and is_tall:
     print("You are a tall male")
+elif is_male and not(is_tall):
+    print("You are a short male")
+elif not(is_male) and is_tall:
+    print("you are not male but you are tall")
 else:
-    print("You are either not male or not tall")
+    print("You are not male or not tall")
