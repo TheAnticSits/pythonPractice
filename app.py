99656c6aa9cae7ff79106e95f01d97669ed92b99
Ken Messmer <kenmessmerjr@gmail.com>
2021-01-03 00:09:49 -0600

    basic calculator using if elif

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 
-def max_num(num1, num2, num3):
-    if num1 >= num2 and num1 >= num3:
-        return num1
+num1 = float(input("Enter first number: "))
+op = (input("Enter operator: "))
+num2 = float(input("Enter second number: "))
 
-    elif num2 >= num1 and num2 >= num3:
-        return num2
-
-    else:
-        return num3
-
-
-print(max_num(300, 40, 5))
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "/":
+    print(num1 / num2)
+elif op == "*":
+    print(num1 * num2)
+else:
+    print("invalid operator")
