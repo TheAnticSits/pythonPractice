7a048c3e99f9f6274371b83446e344e895c9a630
Ken Messmer <kenmessmerjr@gmail.com>
2021-01-02 23:53:16 -0600

    comparison operators in if statements

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 
-is_male = False
-is_tall = True
+def max_num(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
 
-if is_male and is_tall:
-    print("You are a tall male")
-elif is_male and not(is_tall):
-    print("You are a short male")
-elif not(is_male) and is_tall:
-    print("you are not male but you are tall")
-else:
-    print("You are not male or not tall")
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+
+    else:
+        return num3
+
+
+print(max_num(300, 40, 5))
