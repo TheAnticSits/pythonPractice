10459bb609f10b5817a4cbd798d3c1a42e7b628d
Ken Messmer <kenmessmerjr@gmail.com>
2021-01-04 18:46:38 -0600

    dictionaries, key value pairs and .get

--- app.py
+++ app.py
@@ -1,36 +1,17 @@
 
-import turtle
-import random
+monthConversions = {
+    "Jan": "January",
+    "Fed": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
 
-def change_color():
-    R = random.random()
-    B = random.random()
-    G = random.random()
-    turtle.color(R, G, B)
-
-canvas = turtle.Screen()
-turtle.pensize(10)
-
-for i in range(300):
-    i += 11
-    def fun_drawing():
-
-        turtle.forward(i)
-        turtle.right(48)
-        change_color()
-
-        if i % 13 == 0:
-            canvas.bgcolor("red")
-        elif i % 23 == 0:
-            canvas.bgcolor("orange")
-        elif i % 34 == 0:
-            canvas.bgcolor("yellow")
-        elif i % 54 == 0:
-            canvas.bgcolor("purple")
-        elif i % 39 == 0:
-            canvas.bgcolor("green")
-        else:
-            canvas.bgcolor("black")
-
-    fun_drawing()
-
+print(monthConversions.get("Mar", "Not a valid Key"))
