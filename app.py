bafa89761f9cd08b418a14656959ac137c377ecd
Ken Messmer <kenmessmerjr@gmail.com>
2021-01-05 23:13:57 -0600

    while loop in python

--- app.py
+++ app.py
@@ -1,20 +1,7 @@
 
-monthConversions = {
-    "jan": "January",
-    "feb": "February",
-    "mar": "March",
-    "apr": "April",
-    "may": "May",
-    "jun": "June",
-    "jul": "July",
-    "aug": "August",
-    "sep": "September",
-    "oct": "October",
-    "nov": "November",
-    "dec": "December",
-}
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
 
-month = input("Enter a key: ")
-month = month.lower()
-
-print(monthConversions.get(month, "Not a valid Key"))
+print("done")
