ff5f55b67e69714836c8ab2b771d03546eee515f
Ken Messmer <kenmessmerjr@gmail.com>
2021-01-08 00:08:57 -0600

    basic for loop in Python

--- app.py
+++ app.py
@@ -1,18 +1,3 @@
 
-secret_word = input("What is the secret word: ")
-guess = ""
-guess_count = 0
-guess_limit = int(input("How many guesses to you get: "))
-out_of_guesses = False
-
-while guess != secret_word and not(out_of_guesses):
-    if guess_count < guess_limit:
-        guess = input("Enter guess: ")
-        guess_count += 1
-    else:
-        out_of_guesses = True
-
-if out_of_guesses:
-    print("Out of Guesses, You Lose!")
-else:
-    print("You Win!")
+for letter in "Giraffe Academy":
+    print(letter)
