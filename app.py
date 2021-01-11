655f7ffb3a4b0dda785a5b55cd7a00ea8dee645e
Ken Messmer <kenmessmerjr@gmail.com>
2021-01-10 23:54:54 -0600

    2D lists

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 
-def raise_to_power(base_num, pow_num):
-    result = 1
-    for index in range(pow_num):
-        result = result * base_num
-    return result
+number_grid = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [0]
+]
 
-print(raise_to_power(9, 8))
+print(number_grid[0][0])
