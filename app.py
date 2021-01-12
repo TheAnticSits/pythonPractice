2cef7d3e32c996a3059c0e829333bb14720d5b2b
Ken Messmer <kenmessmerjr@gmail.com>
2021-01-11 23:57:05 -0600

    changing specific letters within a string.

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 
-number_grid = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [0]
-]
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter in "aeiouAEIOU":
+            translation = translation + "g"
+        else:
+            translation = translation + letter
 
-for row in number_grid:
-    for col in row:
-        print(col)
+    return translation
+
+
+print(translate(input("enter a phrase")))
