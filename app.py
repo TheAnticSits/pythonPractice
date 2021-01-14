9f87deac8b3446b4369c7273b7bfa0e96326a8a0
Ken Messmer <kenmessmerjr@gmail.com>
2021-01-13 23:31:28 -0600

    practice with Turtle

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 
-try:
-    number = int(input("Enter a number: "))
-    print(number)
-except:
-    print("invalid input")
+import turtle
+turtle.getscreen()
+t = turtle.Turtle()
+motion = 1
+for i in range(50):
+    turtle.down
+    turtle.forward(motion)
+    turtle.right(30)
+    motion += 1
+    turtle.backward(10)
+
