bdb259e84d1248f8ec49e9805a11064189adee2c
Ken Messmer <kenmessmerjr@gmail.com>
2021-01-13 23:58:54 -0600

    Working with width in turtle

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 
 import turtle
+import random
+
 turtle.getscreen()
 t = turtle.Turtle()
 motion = 1
-for i in range(50):
+wide = random.randrange(20, 50, 50)
+
+def change_color():
+    R = random.random()
+    B = random.random()
+    G = random.random()
+
+    turtle.color(R, G, B)
+
+
+for i in range(200):
+    turtle.width(wide)
     turtle.down
     turtle.forward(motion)
     turtle.right(30)
     motion += 1
     turtle.backward(10)
+    change_color()
+
 
