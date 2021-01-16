6c6b82a7f8cda00b1a9ec3aa79ae72c6afe1c84a
Ken Messmer <kenmessmerjr@gmail.com>
2021-01-16 17:27:36 -0600

    adding text file

--- app.py
+++ app.py
@@ -1,49 +1,11 @@
 
-import turtle
-import random
 
-turtle.getscreen()
-t = turtle.Turtle()
+employee_file = open("if", "r")
 
-def move_turtle():
-    move = random.random()
-    return move
+print(employee_file.readable())
 
-def change_color():
-    R = random.random()
-    B = random.random()
-    G = random.random()
+employee_file.close()
 
-    turtle.color(R, G, B)
-
-
-def move_that_turtle():
-
-    motion = 1
-
-    for j in range(10):
-        turtle.up()
-        turtle.forward(motion)
-        turtle.forward(motion)
-        turtle.forward(motion)
-        turtle.forward(motion)
-        turtle.left(motion)
-        if j % 5 == 0:
-            motion = 1
-            turtle.right(40)
-            turtle.forward(50)
-
-        turtle.down()
-
-        for i in range(25):
-            turtle.width(15)
-            turtle.forward(motion)
-            change_color()
-            turtle.right(115)
-            motion += 1
-            turtle.backward(motion)
-            change_color()
-            turtle.left(random.random())
-
-
-move_that_turtle()
+# "r" is for read
+# "w" is for write
+# "a" is for append
