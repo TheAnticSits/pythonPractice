0cb480e11dc0a6d56146d0186e15a46e85ef2441
Ken Messmer <kenmessmerjr@gmail.com>
2021-01-18 16:47:13 -0600

    import functionality from external python files

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
 
+import useful_tools
 
-employee_file = open("index.html", "w")
-
-employee_file.write("<p>This is HTML<p>")
-
-
-employee_file.close()
-
-# "r" is for read
-# "w" is for write
-# "a" is for append
+print(useful_tools.roll_dice(10))
